a7376eb2e73eec423a0893ff118c9704d57b6766
HenryW <henryracoomb@gmail.com>
2024-07-02 10:48:31 -0400

    Created first 3 get routes

--- guided_project_2/server/server.js
+++ guided_project_2/server/server.js
@@ -8,6 +8,8 @@ dotenv.config();
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
 const planetCollectionName = process.env.MONGO_DB_PLANETS;
+const filmsCollectioName = process.env.MONGO_DB_FILMS;
+const charactersCollectionName = process.env.MONGO_DB_CHARACTERS;
 
 
 const app = express();
@@ -15,18 +17,35 @@ app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Middleware to parse JSON bodies
 const PORT = 3000;
 
-// api/planets get 
-app.get('/planets', async (req, res) => {
+// api/characters get 
+app.get('/characters', async (req, res) => {
     try {
         const client = await MongoClient.connect(url);
         const db = client.db(dbName);
-        const collection = db.collection(planetCollectionName);
-        const planet = await collection.find({}).toArray();
+        const collection = db.collection(charactersCollectionName);
+        //const collection = db.collection('characters');
+        const chars = await collection.find({}).toArray();
 
-        res.json(planet);
+        res.json(chars);
     } catch (err) {
         console.error("Error:", err);
-        res.status(500).send("PLANETS NOT FOUND");
+        res.status(500).send("CHARACTERS/GET NOT FOUND");
+    }
+});
+
+// api/films get 
+app.get('/films', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(filmsCollectioName);
+        //const collection = db.collection('films');
+        const films = await collection.find({}).toArray();
+
+        res.json(films);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("FILMS/GET NOT FOUND");
     }
 });
 
@@ -41,22 +60,7 @@ app.get('/planets', async (req, res) => {
         res.json(planet);
     } catch (err) {
         console.error("Error:", err);
-        res.status(500).send("PLANETS NOT FOUND");
-    }
-});
-
-// api/planets get 
-app.get('/planets', async (req, res) => {
-    try {
-        const client = await MongoClient.connect(url);
-        const db = client.db(dbName);
-        const collection = db.collection(planetCollectionName);
-        const planet = await collection.find({}).toArray();
-
-        res.json(planet);
-    } catch (err) {
-        console.error("Error:", err);
-        res.status(500).send("PLANETS NOT FOUND");
+        res.status(500).send("PLANETS/GET NOT FOUND");
     }
 });
 
