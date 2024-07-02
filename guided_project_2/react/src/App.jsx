9568582d63f65e64f5101632172c2ff2222736ac
HenryW <henryracoomb@gmail.com>
2024-07-02 14:35:20 -0400

    aaaaa

--- guided_project_2/react/src/App.jsx
+++ guided_project_2/react/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Characters from "./components/Characters"
+import Character from "./components/Character"
+
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Star Wars Universe Lookup</h1>
+        <label htmlFor="searchString">Who you looking for?(Regular expressions are cool
+            here)</label>
+       </div>
+        
+      <Routes>
+        <Route path="/" element={<Characters />}/>
+        <Route path="/character/:id" element = {<Character />} />
+
+      </Routes>
+    </BrowserRouter>
+        
+
     </>
   )
 }
