442800e1806773688d48f3fcb3b7faf8647ccf7d
HenryW <henryracoomb@gmail.com>
2024-07-02 10:36:53 -0400

    Added a few get listeners, decided to add .env files

--- guided_project_2/server/server.js
+++ guided_project_2/server/server.js
@@ -7,26 +7,56 @@ import cors from 'cors';
 dotenv.config();
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
-const planetCollectionName = process.env.MONG_DB_PLANETS;
+const planetCollectionName = process.env.MONGO_DB_PLANETS;
+
 
 const app = express();
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Middleware to parse JSON bodies
 const PORT = 3000;
 
-// Endpoint to read and send JSON file content
+// api/planets get 
 app.get('/planets', async (req, res) => {
     try {
         const client = await MongoClient.connect(url);
         const db = client.db(dbName);
         const collection = db.collection(planetCollectionName);
-        //const socks = await collection.find({}).toArray();
-        const planet = {id: "test object", name: "generic planet"}
+        const planet = await collection.find({}).toArray();
 
         res.json(planet);
     } catch (err) {
         console.error("Error:", err);
-        res.status(500).send("PLANETS NO FOUND");
+        res.status(500).send("PLANETS NOT FOUND");
+    }
+});
+
+// api/planets get 
+app.get('/planets', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(planetCollectionName);
+        const planet = await collection.find({}).toArray();
+
+        res.json(planet);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("PLANETS NOT FOUND");
+    }
+});
+
+// api/planets get 
+app.get('/planets', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(planetCollectionName);
+        const planet = await collection.find({}).toArray();
+
+        res.json(planet);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("PLANETS NOT FOUND");
     }
 });
 
